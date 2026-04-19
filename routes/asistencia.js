7c9709c5588e1975bf8979e5335d5152baede5c7
Gregorio Flores <gregoriofloress5498@gmail.com>
2026-04-19 01:43:15 -0600

    ubicacion corregida

--- routes/asistencia.js
+++ routes/asistencia.js
@@ -6,8 +6,8 @@ import Alumno from "../models/Alumno.js";
 const router = express.Router();
 
 // coordenadas escuela
-const LAT_ESCUELA = 19.29856;
-const LNG_ESCUELA = -98.95583;
+const LAT_ESCUELA = 19.29810;
+const LNG_ESCUELA = -98.95495;
 
 // calcular distancia
 function calcularDistancia(lat1, lon1, lat2, lon2){
